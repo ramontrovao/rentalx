7585367990ffe22d231f492c5ded256da8aa9d66
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-02-26 19:34:55 -0300

    docs: added docs to /categories route with swagger (#11)

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -25,6 +25,7 @@ export class ImportCategoryUseCase {
           this.categories.push({ name, description });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(this.categories);
         })
         .on("error", (err) => {
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
